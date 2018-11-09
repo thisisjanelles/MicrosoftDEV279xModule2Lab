8284b7f315ca24347d45bcb717957e43148be020
Janelle <muziqlover025@gmail.com>
2018-11-09 13:18:49 -0500

    duplicate guesses

--- game.js
+++ game.js
@@ -5,13 +5,27 @@ var limit = 5;
 var remaining = limit;
 var won = false;
 var guesses = [];
+var previous;
+
+function isPreviousGuess() {
+    for (i = 0; i < guesses.length; i++) {
+        if (guesses[i] === guess) {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 for (i = 0; i < limit; i++) {
     // prompt user for their guess
 
-    guess = window.prompt("Guess a number!");
-    guess = Number(guess);
-    
+    do {
+        guess = window.prompt("Guess a number!");
+        guess = Number(guess);
+        previous = isPreviousGuess();
+    } while (previous === true);
+
     // if correct: let the user know they won
     
     // if incorrect: let the user know
@@ -21,8 +35,9 @@ for (i = 0; i < limit; i++) {
         won = true;
         break;
     } else {
+        guesses[i] = guess;
         remaining--;
-        window.alert("Try again! You have " + remaining + " tries remaining.");
+        window.alert("Incorrect! You have " + remaining + " tries remaining. \nPrevious guesses: " + guesses.toString());
     }
 }
 
